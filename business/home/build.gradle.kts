9aab2cc70eaa5960281b567b4ebf8640bd42aea1
komamj <junmaokoma@gmail.com>
2021-11-13 15:01:06 +0800

    feat: [home] #N/A add home moudle.

---
+++ business/home/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+kapt {
+    arguments {
+        arg("AROUTER_MODULE_NAME", project.name)
+    }
+}
+
+android {
+    compileSdk = Versions.compileSdk
+    buildToolsVersion = Versions.buildTools
+
+    defaultConfig {
+        minSdk = Versions.minSdk
+        targetSdk = Versions.targetSdk
+
+        resourcePrefix("home_")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.compose
+    }
+
+    lint {
+        isAbortOnError = true
+        isIgnoreWarnings = true
+        lintConfig = rootProject.file("lint.xml")
+    }
+}
+
+dependencies {
+    arouter()
+    dagger()
+    daggerAndroidX()
+
+    compose()
+
+    api(project(":home_api"))
+    implementation(project(":auth_api"))
+    implementation(project(":common"))
+    implementation(Dependencies.datastore_preferences)
+}
---
+++ business/home_api/src/main/java/com/komamj/business/home/api/HomeService.kt
@@ -0,0 +1,6 @@
+package com.komamj.business.home.api
+
+import com.alibaba.android.arouter.facade.template.IProvider
+
+interface HomeService : IProvider {
+}

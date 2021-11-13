53df29eec097cd9067c8794cb7e8e8cdb583eca6
komamj <junmaokoma@gmail.com>
2021-11-13 15:07:20 +0800

    feat: [home] #N/A 添加home与其他组件间能力调用的解耦抽象服务。

--- business/home_api/src/main/java/com/komamj/business/home/api/HomeService.kt
+++ business/home_api/src/main/java/com/komamj/business/home/api/HomeService.kt
@@ -1,6 +1,11 @@
 package com.komamj.business.home.api
 
+import androidx.fragment.app.Fragment
 import com.alibaba.android.arouter.facade.template.IProvider
 
 interface HomeService : IProvider {
+    /**
+     * 提供首页 Fragment
+     */
+    fun provideHomeFragment(): Fragment
 }

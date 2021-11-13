437357bc650d01375743b9340051f540dfb37ef8
komamj <junmaokoma@gmail.com>
2021-11-13 15:07:20 +0800

    feat: [home] #N/A 添加home与其他组件间能力调用的解耦抽象服务。

---
+++ business/home/src/main/java/com/komamj/business/home/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.komamj.business.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+}
--- business/home/src/main/java/com/komamj/business/home/HomeServiceImpl.kt
+++ business/home/src/main/java/com/komamj/business/home/HomeServiceImpl.kt
@@ -1,4 +1,13 @@
 package com.komamj.business.home
 
-class HomeServiceImpl {
+import android.content.Context
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.komamj.business.home.api.HomeService
+
+@Route(path = "home/main")
+class HomeServiceImpl : HomeService {
+    override fun init(context: Context?) {
+    }
+
+    override fun provideHomeFragment() = HomeFragment.newInstance()
 }
